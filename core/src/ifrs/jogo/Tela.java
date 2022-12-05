dd9530a82c9f6501198213161da52d5d69b35daf
Cassiano Sobierai <77468989+cassisob@users.noreply.github.com>
2022-12-05 14:54:54 -0300

    modificando o mapa

--- core/src/ifrs/jogo/Tela.java
+++ core/src/ifrs/jogo/Tela.java
@@ -1,12 +1,18 @@
 package ifrs.jogo;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.Map;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 import java.lang.management.ThreadMXBean;
 
@@ -16,13 +22,24 @@ public class Tela extends InputAdapter implements Screen {
     private World mundo;
     private TmxMapLoader carregarMapa;
     private TiledMap mapa;
+    private OrthogonalTiledMapRenderer carregador;
+    private OrthographicCamera gamecam;
+    private Viewport gamePort;
+    private SpriteBatch batch;
+
 
     public Tela(Game game) {
 
+        batch = new SpriteBatch();
+
         mundo = new World(new Vector2(0, -100f), true);
+
+        gamecam = new OrthographicCamera();
+        gamePort = new FitViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), gamecam);
 
         carregarMapa = new TmxMapLoader();
         mapa = carregarMapa.load("mapa.tmx");
+        carregador = new OrthogonalTiledMapRenderer(mapa);
 
     }
 
@@ -33,12 +50,30 @@ public class Tela extends InputAdapter implements Screen {
 
     @Override
     public void render(float delta) {
+        update(delta);
 
+        batch.setProjectionMatrix(gamecam.combined);
+
+        batch.begin();
+
+        carregador.render();
+
+        batch.end();
+
+    }
+
+    public void update(float delta) {
+        mundo.step(1 / 60f, 6, 2);
+
+        gamecam.position.x = Gdx.input.getX();
+
+        gamecam.update();
+        carregador.setView(gamecam);
     }
 
     @Override
     public void resize(int width, int height) {
-
+        gamePort.update(width, height);
     }
 
     @Override

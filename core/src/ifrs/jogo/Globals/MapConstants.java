6801d3ea9d8a35c1d859508590e96da8b55d7297
Gabriel L <gabriellovato15@gmail.com>
2023-01-04 15:51:59 -0300

    Alteração nos globais

--- core/src/ifrs/jogo/Globals/MapConstants.java
+++ core/src/ifrs/jogo/Globals/MapConstants.java
@@ -1,10 +1,10 @@
 package ifrs.jogo.Globals;
 
 public class MapConstants {
-    public static int FUNDO         = 0;
+	public static int FUNDO         = 0;
 	public static int DETALHE1		= 1;
 	public static int DETALHE2		= 2;
-    public static int COLISAO       = 3;
-    public static int PORTA			= 4;
-    public static int INIMIGO       = 5;
+	public static int COLISAO       = 3;
+	public static int PORTA			= 4;
+	public static int INIMIGO       = 5;
 }

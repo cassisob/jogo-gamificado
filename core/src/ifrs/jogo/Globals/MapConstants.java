3bf5b2a68e7a02a22c4f72f3b631ebf90efe3b0c
Gabriel L <gabriellovato15@gmail.com>
2023-01-04 15:49:22 -0300

    Mudança na spritesheet, mapas novos, alteração nas constantes

--- core/src/ifrs/jogo/Globals/MapConstants.java
+++ core/src/ifrs/jogo/Globals/MapConstants.java
@@ -2,7 +2,9 @@ package ifrs.jogo.Globals;
 
 public class MapConstants {
     public static int FUNDO         = 0;
-    public static int COLISAO       = 1;
-    public static int INTERAGIVEL   = 2;
-    public static int INIMIGO       = 3;
+	public static int DETALHE1		= 1;
+	public static int DETALHE2		= 2;
+    public static int COLISAO       = 3;
+    public static int PORTA			= 4;
+    public static int INIMIGO       = 5;
 }
